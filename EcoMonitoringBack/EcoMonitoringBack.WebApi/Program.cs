afb9463fb5bb4eebd6927054265ea4a4ad576a07
Stas <stanislav.ivanov.2004@internet.ru>
2025-11-12 16:44:06 +0500

    убрал чуть лишнего

--- EcoMonitoringBack/EcoMonitoringBack.WebApi/Program.cs
+++ EcoMonitoringBack/EcoMonitoringBack.WebApi/Program.cs
@@ -62,39 +62,6 @@ app.UseRouting();
 app.UseHttpsRedirection();
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    
-    try
-    {
-        var context = services.GetRequiredService<IRepositoryContainers>();
-        
-        // Проверяем, пустая ли БД
-        if (!await context.IsHaveDbContainersAsync())
-        {
-            var migrationService = services.GetRequiredService<IServiceMigrationContainer>();
-            var filePath = "file.xlsx";
-            
-            if (File.Exists(filePath))
-            {
-                using var stream = File.OpenRead(filePath);
-                var file = new FormFile(stream, 0, stream.Length, null, Path.GetFileName(filePath))
-                {
-                    Headers = new HeaderDictionary(),
-                    ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                };
-                
-                await migrationService.StartMigrationAsync(file);
-                Console.WriteLine("ееееееееееееееее боиии");
-            }
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine("Всо сломалось");
-        throw;
-    }
-}
+app.MakeContainersMigration();
 
 app.Run();
---
+++ EcoMonitoringBack/EcoMonitoringBack.WebApi/SetupApplication.cs
@@ -0,0 +1,46 @@
+﻿using EcoMonitoringBack.Interfaces;
+
+namespace EcoMonitoringBack;
+
+
+public static class SetupApplication
+{
+    public static async void MakeContainersMigration(
+        this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+    
+            try
+            {
+                var context = services.GetRequiredService<IRepositoryContainers>();
+        
+                // Проверяем, пустая ли БД
+                if (!await context.IsHaveDbContainersAsync())
+                {
+                    var migrationService = services.GetRequiredService<IServiceMigrationContainer>();
+                    var filePath = "file.xlsx";
+            
+                    if (File.Exists(filePath))
+                    {
+                        using var stream = File.OpenRead(filePath);
+                        var file = new FormFile(stream, 0, stream.Length, null, Path.GetFileName(filePath))
+                        {
+                            Headers = new HeaderDictionary(),
+                            ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                        };
+                
+                        await migrationService.StartMigrationAsync(file);
+                        Console.WriteLine("ееееееееееееееее боиии");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Всо сломалось");
+                throw;
+            }
+        }
+    }
+}
